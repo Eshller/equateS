2b7c77e2fcab7827af2e3af025b707727ec5bd3a
kirtann <94744255+kirtann@users.noreply.github.com>
2023-11-10 14:24:24 +0530

    mobile responsiveness of landing page

--- src/pages/Landing/Satellite.jsx
+++ src/pages/Landing/Satellite.jsx
@@ -4,20 +4,25 @@ import  Satellites from '../../assets/Satellites.svg';
 const Satellite = () => {
   return (
     <div
-      className="p-4 w-full -z-30"
+      className="p-4 w-full flex-col block -z-30"
     >
 
       {/* Planet */}
       <div
-        className="md:h-screen flex flex-col w-full h-full text-white items-center justify-center z-10"
+        className="md:h-[100vh] flex flex-col w-full h-[70vh] text-white items-center justify-center z-10"
       >
       </div>
 
       <div className="h-0">
-        <div className="absolute top-[-90vh] md:top-[120vh]  bottom-[90vh] md:left-[20vw] lg:w-[70vw] lg:h[70vh] md:w-[70vh] md:h-[70vh] w-[70vw] h-[70vh] items-center justify-center flex z-0 ">
+        <div className="absolute top-[110vh] left-[9vh]  md:top-[120vh]  md:left-[13vw] lg:left-[30vw] md:w-[70vh] md:h-[70vh] w-[90vw] h-[90vh] items-center justify-center flex z-0 ">
               <div
-                className="relative md:top-0 w-full h-full md:w-full md:h-full">
-                <img className='absolute lg:w-[110vw] lg:h[110vh] md:w-[110vh] md:h-[110vh] lg:left-[-5vw] md:left-[10vw] w-[90vw] h-[90vh] items-center justify-center z-0 ' src={Satellites} />
+                className="absolute top-[0vh] left-[-10vw] md:top-0 h-[56vh] w-[86vw] sm:h-[80vh] sm:w-[86vw] md:w-[100vh] md:h-[110vh] md:left-[-5vw]"
+                style={{
+                  backgroundImage: `url(${Satellites})`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                }}>
+                {/* <img className='absolute  md:w-[110vh] md:h-[110vh] md:left-[10vw] w-[70vw] h-[90vh] items-center justify-center z-0 ' src={Satellites} /> */}
               </div>
         </div>
       </div>
